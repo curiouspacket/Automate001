cc66e951f5b0ed583bbd0579445f7c4a3183f3ee
aaronshapirotech@gmail.com <aaronshapirotech@gmail.com>
2020-02-04 12:04:58 -0700

    IP Address fun

--- new_auto.py
+++ new_auto.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 
+import sys
+from argparse import ArgumentParser
+
+def get_ip_info():
+    do = False
+    while do == False:
+        ip ={}
+        ip['address'] = input("What IP address do you want to set? ")
+        ip['mask'] = input("What Subnet Mask do you want to set? ")
+        yn = input("Would you like to add more(Y//N) ? ")
+        if yn == 'N':
+            do = True
+    return(ip)
+
 def main():
     print("Hellow World")
-
-
+    ip = get_ip_info()
+    print(('IP: {address} \n SM: {mask}').format(**ip))
 if __name__ == '__main__':
     main()
